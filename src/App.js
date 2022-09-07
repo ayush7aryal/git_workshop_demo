495d660aa7e4b9de74948808ee20ba77ea10422e
ayush7aryal <ayush7aryal@gmail.com>
2022-09-07 15:55:02 +0545

    some datas added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import './App.css';
-import { Names } from './names/data';
+import "./App.css";
+import { Names } from "./names/data";
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <h1 className="title">
+        WORKSHOP ON VERSION CONTROL SYSTEM WITH GIT AND GITHUB
+      </h1>
+      <h3>Details of users who have completed PR request</h3>
       <div className="cards">
         {Names.map((data) => {
-          return (<div className="card-content">
-            <div className="name">{data.name}</div>
-            <div className="class">Class :{data.class}</div>
-            <div className="rollno">Roll No: {data.rollno}</div>
-          </div>)
+          return (
+            <div className="card-content">
+              <div className="name">{data.name}</div>
+              <div className="class">Class :{data.class}</div>
+              <div className="rollno">Roll No: {data.rollno}</div>
+            </div>
+          );
         })}
       </div>
     </div>
--- src/names/data.js
+++ src/names/data.js
@@ -24,4 +24,29 @@ export const Names = [
     class: "17",
     rollno: "13",
   },
+  {
+    name: "ashish thapa",
+    class: "10",
+    rollno: "28",
+  },
+  {
+    name: "Ayush Aryal",
+    class: "10",
+    rollno: "23",
+  },
+  {
+    name: "Avaya Bajracharya",
+    class: "10",
+    rollno: "20",
+  },
+  {
+    name: "Ayush Paudel",
+    class: "12",
+    rollno: "13",
+  },
+  {
+    name: "Shubhechchhak",
+    class: "17",
+    rollno: "13",
+  },
 ];
