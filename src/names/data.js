8a0186ca07acf732dcd5db3329f81a28740c5e47
Mukesh Tiwari <mukeshtiwari200102@gmail.com>
2022-09-08 17:27:00 +0545

    my changes

--- src/names/data.js
+++ src/names/data.js
@@ -5,4 +5,10 @@ export const Names = [
     semester: "5th semester",
     message: "If you are not there today, it doesn't mean that you won't be there tommorow!" 
   },
+  {
+      name: 'Mukesh Tiwari',
+      department: 'Computational Mathematics',
+      semester: '5th sem',
+      message: 'Dubai trainers exceeded expectations by a ton.'
+  }
 ];
