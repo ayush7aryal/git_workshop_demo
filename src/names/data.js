fb439a7309cbc6a87e672c04b3410b09421abd3f
prastut <prastutbhattarai88@gmail.com>
2022-09-08 17:10:36 +0545

    ok commit

--- src/names/data.js
+++ src/names/data.js
@@ -5,4 +5,10 @@ export const Names = [
     semester: "5th semester",
     message: "If you are not there today, it doesn't mean that you won't be there tommorow!" 
   },
+  {
+    name: "prastut bhattarai",
+    department: "Computer Engineering",
+    semester: "3th semester",
+    message: "If you are not there today, it doesn't mean that you won't be there tommorow!" 
+  },
 ];
