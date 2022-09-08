5e203a15810702d4f0148e648db8399735e90cb3
critmis <44994399+ayambasyal@users.noreply.github.com>
2022-09-08 17:12:44 +0545

    try

--- src/names/data.js
+++ src/names/data.js
@@ -5,4 +5,10 @@ export const Names = [
     semester: "5th semester",
     message: "If you are not there today, it doesn't mean that you won't be there tommorow!" 
   },
+  {
+    name: "suoriya baniya",
+    department: "Computational mathematics",
+    semester: "3rd semester",
+    message: "dhum tananananana dhum tananananana dhum tananananana" 
+  },
 ];
