09a3ecba74dfd2da6840d8bd842fc2266f224604
vivefer <vivekanandag38@gmail.com>
2022-09-08 17:13:00 +0545

    first public

--- src/names/data.js
+++ src/names/data.js
@@ -5,4 +5,11 @@ export const Names = [
     semester: "5th semester",
     message: "If you are not there today, it doesn't mean that you won't be there tommorow!" 
   },
+
+  {
+    name: "Vivekananda Gupta",
+    department: "Computational Mathematics",
+    semester: "2nd semester",
+    message: "test message" 
+  },
 ];
