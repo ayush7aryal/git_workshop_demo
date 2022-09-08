bcf47d80333e016b854e7a2f35e8cad35bda5109
Zavedkrishna <113102929+Zavedkrishna@users.noreply.github.com>
2022-09-08 17:22:16 +0545

    Mw Zac

--- src/names/data.js
+++ src/names/data.js
@@ -5,4 +5,10 @@ export const Names = [
     semester: "5th semester",
     message: "If you are not there today, it doesn't mean that you won't be there tommorow!" 
   },
+{
+    name: "Zaved Swikar",
+    department: "He Man Suii Engineering",
+    semester: "2nd semester",
+    message: "If you are not there today, it  mean that you won't be there tommorow!" 
+  },
 ];
