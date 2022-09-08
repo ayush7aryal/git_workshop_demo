2a2783635e5b8ebe1f320950e1df9ef92bab7b2b
ankurbaral <ankurbaral@gmail.com>
2022-09-08 17:15:49 +0545

    updated data by baral

--- src/names/data.js
+++ src/names/data.js
@@ -5,4 +5,10 @@ export const Names = [
     semester: "5th semester",
     message: "If you are not there today, it doesn't mean that you won't be there tommorow!" 
   },
+  {
+    name: "Ankur Baral",
+    department: "Computational Mathematics",
+    semester: "3rd semester",
+    message: "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me." 
+  },
 ];
